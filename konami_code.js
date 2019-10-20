49838ad04959d0986a60279cb5db46aadf303ab9
free-writer-5650 <miles.higbie@gmail.com>
2019-10-20 15:18:45 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,30 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+let index = 0; 
+
+let miles = document.body; 
+
+miles.addEventListener("keydown",function(e) {
+  const key = e.key;
+  console.log(key);
+  
+  if(key === codes[index]){
+    ++index; 
+    
+  if(index === codes.length){ 
+    alert("Hurray!"); 
+    
+    index = 0; 
+  } 
+  } else { 
+  
+    index = 0; 
+    
+  }
+  
+});
+
+
+ 
+
